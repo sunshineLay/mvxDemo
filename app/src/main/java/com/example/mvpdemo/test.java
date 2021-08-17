2286c31a2be576ab158c928d7abaade18bffcbe6
2491812016@qq.com <>
2021-08-17 18:00:35 +0800

    保存

---
+++ app/src/main/java/com/example/mvpdemo/test.java
@@ -0,0 +1,17 @@
+package com.example.mvpdemo;
+
+/**
+ * @TODO:
+ * @Date: 2021/8/17 17:39
+ * @User: lay
+ */
+public class test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println();
+    }
+    static class Student{
+        String name = "hahahhahh，哈哈哈哈哈";
+        int age;
+    }
+}
